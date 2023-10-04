0cea8ab1b5ce89c100be6a9405f0ee9806fb6f59
beteland123 <beteland23@gmail.com>
2023-10-04 19:42:32 +0300

    Add test for payment model

---
+++ spec/models/payment_spec.rb
@@ -0,0 +1,70 @@
+require 'rails_helper'
+
+RSpec.describe Payment, type: :model do
+  let(:user) do
+    User.create(
+      name: 'John Doe',
+      email: 'john@example.com',
+      password: 'password123',
+      password_confirmation: 'password123'
+    )
+  end
+
+  let(:category) do
+    Category.new(
+      name: 'Fast Food',
+      icon: 'imgexample.png',
+      user: user
+    )
+  end
+
+  it 'is valid with valid attributes' do
+    payment = Payment.new(
+      name: 'Burger',
+      amount: 1.5,
+      user: user,
+      category: category
+    )
+    expect(payment).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    payment = Payment.new(
+      amount: 1.5,
+      user: user,
+      category: category
+    )
+    expect(payment).to_not be_valid
+  end
+
+  it 'is not valid without an amount' do
+    payment = Payment.new(
+      name: 'Burger',
+      user: user,
+      category: category
+    )
+    expect(payment).to_not be_valid
+  end
+
+  describe 'associations' do
+    it 'belongs to user' do
+      payment = Payment.new(
+        name: 'Burger',
+        amount: 1.5,
+        user: user,
+        category: category
+      )
+      expect(payment.user).to eq(user)
+    end
+
+    it 'belongs to category' do
+      payment = Payment.new(
+        name: 'Burger',
+        amount: 1.5,
+        user: user,
+        category: category
+      )
+      expect(payment.category).to eq(category)
+    end
+  end
+end
