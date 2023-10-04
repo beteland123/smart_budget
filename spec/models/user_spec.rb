333714268ba18927334d6e2e8cb9bd1deb13f1d2
beteland123 <beteland23@gmail.com>
2023-10-04 18:54:17 +0300

    Add test for user model associations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,5 +19,14 @@ RSpec.describe User, type: :model do
     )
     expect(user).to_not be_valid
   end
+  describe 'associations' do
+    it 'has many categories' do
+      expect(User.reflect_on_association(:categories).macro).to eq(:has_many)
+    end
+  
+    it 'has many payments' do
+      expect(User.reflect_on_association(:payments).macro).to eq(:has_many)
+    end
+  end
  
 end
